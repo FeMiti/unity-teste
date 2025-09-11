01e5d4821f2694b7bbdc134bad97149b58142b33
FeMiti <felipe.mitidieri@gmail.com>
2025-09-11 10:50:24 -0300

    Textura e Script pra Pico-chan

---
+++ Teste/Assets/Scripts/SimpleRotation.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 1f, 0f);
+    }
+}
